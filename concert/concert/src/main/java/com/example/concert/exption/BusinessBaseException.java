3be3060bcbd804b43d8f300bc6734a3888fe7aab
Taehwan2 <taehwansw@gmail.com>
2024-07-17 23:09:16 +0900

    Exception 코드 처리 완료

--- concert/concert/src/main/java/com/example/concert/exption/BusinessBaseException.java
+++ concert/concert/src/main/java/com/example/concert/exption/BusinessBaseException.java
@@ -1,19 +1,23 @@
 package com.example.concert.exption;
 
+//Todo RuntimeException을 상속한 개별 Exception 추가
 public class BusinessBaseException extends RuntimeException{
-
+    //Todo 에러코드 에러코드 의존성 주입
     private final ErrorCode errorCode;
 
+    //Todo 에러코드와 message 가 같이 들어올 경우
     public BusinessBaseException(ErrorCode errorCode,String message) {
         super(message);
         this.errorCode = errorCode;
     }
 
+    //Todo 에러코드 의존성 주입
     public BusinessBaseException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
 
+    //Todo  ErrorCode
     public ErrorCode getErrorCode(){
         return errorCode;
     }
--- concert/concert/src/main/java/com/example/concert/exption/GlobalExceptionHandler.java
+++ concert/concert/src/main/java/com/example/concert/exption/GlobalExceptionHandler.java
@@ -8,27 +8,38 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
+
+//예외 처리를 잡을 Handler
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class) // HttpRequestMethodNotSupportedException 예외를 잡아서 처리
+    //Http method가 연결이 안되었을 때 타는 Exception
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class) // HttpRequestMethodNotSupportedException 예외를 잡아서 처리하는 코드
     protected ResponseEntity<ErrorResponse> handle(HttpRequestMethodNotSupportedException e) {
+
+        //error 잡는 로그
         log.error("HttpRequestMethodNotSupportedException", e);
         return createErrorResponseEntity(ErrorCode.METHOD_NOT_ALLOWED);
     }
-
+    //실제 비지니스 로직이 에러가 났을 때 잡는 메서드
     @ExceptionHandler(BusinessBaseException.class)
     protected ResponseEntity<ErrorResponse> handle(BusinessBaseException e) {
+
+        //error 잡는 로그
         log.error("BusinessException", e);
         return createErrorResponseEntity(e.getErrorCode());
     }
 
+    //Server error 가 났을 때 잡는 메서드
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErrorResponse> handle(Exception e) {
+
+        //error 잡는 로그
         e.printStackTrace();
         log.error("Exception", e);
         return createErrorResponseEntity(ErrorCode.INTERNAL_SERVER_ERROR);
     }
 
+    //error code 를 통해서 ResponseEntity를 만드는 코드
     private ResponseEntity<ErrorResponse> createErrorResponseEntity(ErrorCode errorCode) {
         return new ResponseEntity<>(
                 ErrorResponse.of(errorCode),
