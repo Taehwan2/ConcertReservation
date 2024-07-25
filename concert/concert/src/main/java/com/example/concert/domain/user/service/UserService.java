32c2ca258eee4ea3da4a6f0f7fbf4971465c51ab
Taehwan2 <97010824+Taehwan2@users.noreply.github.com>
2024-07-25 20:17:47 +0900

    Update UserService.java

--- concert/concert/src/main/java/com/example/concert/domain/user/service/UserService.java
+++ concert/concert/src/main/java/com/example/concert/domain/user/service/UserService.java
@@ -16,7 +16,7 @@ public class UserService {
    public User getUserPoint(Long userId){
        return  userRepository.getUserPoint(userId);
    }
-
+    //이번 과제의 핵심 비관적 락을 거는 메서드  
     public User getUserWithLock(Long userId){return userRepository.getUserPointWithLock(userId);}
     //유저를 저장하는 로직
     public User save(User userPoint) {
@@ -26,12 +26,12 @@ public class UserService {
         return userRepository.updateUserPoint(userPoint);
     }
 
-
+    //가장 작은 단위로 유저를 조회하고 포인트를 변경하는 것을 트랜잭션으로 묶음
     @Transactional
     public int calculate(Long userId, BigDecimal amount) throws Exception {
         var userPoint = getUserWithLock(userId);
         userPoint.calculate(amount);
-        //계산된 포인트를 저장하고 결과를  pointHistory 저장하는 로직
+        //비관적락을 획득한 순서로 부터 순차로 업데이트
         return updateUserPoint(userPoint);
     }
 
